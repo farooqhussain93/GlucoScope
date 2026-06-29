04d6bfdb99405e59c08aab8560ccb0e074f8fab2
Farooq Hussain <farooqhussain9393@gmail.com>
2026-06-30 02:42:58 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip setuptools wheel
+pip install --prefer-binary -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
